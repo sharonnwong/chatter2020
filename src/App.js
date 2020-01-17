caf9f0c9c134e22dc127145df2411ab3125738f4
Sharon Wong <shachi19@uw.edu>
2020-01-17 09:04:54 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <main>
+    <header> 
+      <img 
+        alt="icon"
+        src="https://upload.wikimedia.org/wikipedia/en/e/e0/WPVG_icon_2016.svg"/>
+      Chatter 
+    </header>
+
+    <TextInput onSend={t=> console.log(t)}/>
+
+    {/*<Hi a={1} b={'2'} />*/}
+  </main>
+}
+
+function TextInput(props) {
+  const [text, setText] = useState('')
+  return <div className="text-input-wrap">
+    <input className="text-input"
+      value={text}
+      placeholder = "Write your message"
+      onChange = {e=> setText(e.target.value)}
+    />
+    <button className="button"
+      onClick={()=> 
+      {props.onSend(text) 
+      setText('')
+    }}> <img alt="send" src="https://upload.wikimedia.org/wikipedia/commons/6/61/Black_Up_Arrow.png"/> </button>
+  </div>
 }
 
 export default App;
