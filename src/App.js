7a5130e360b0cf5f1a600986ede9b350769ba2ea
sharonnwong <shachi19@uw.edu>
2020-02-05 08:53:18 -0800

    final done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React, {useState, useEffect} from 'react';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import './App.css'
+import './media.css'
 import {db, useDB} from './db'
 import NamePicker from './namePicker'
 import { BrowserRouter, Route } from 'react-router-dom'
+import Camera from 'react-snap-pic'
+import {FiCamera} from 'react-icons/fi'
+import * as firebase from "firebase/app"
+import "firebase/storage"
+import Div100vh from 'react-div-100vh'
 
-function App() {
+
+function App(){
   useEffect(()=>{
     const {pathname} = window.location
     if(pathname.length<2) window.location.pathname='home'
-  })
+  }, [])
   return <BrowserRouter>
-    <Route path="/:room" component={Room}/>
+    <Route path="/:room" component={Room} />
   </BrowserRouter>
 }
 
@@ -19,8 +26,21 @@ function Room(props) {
   const {room} = props.match.params
   const [name,setName] = useState("")
   const messages = useDB(room)
+  const [showCamera, setShowCamera] = useState(false)
+
+  async function takePicture(img) {
+    setShowCamera(false)
+    const imgID = Math.random().toString(36).substring(7)
+    var storageRef = firebase.storage().ref()
+    var ref = storageRef.child(imgID + '.jpg')
+    await ref.putString(img, 'data_url')
+    db.send({ 
+      img: imgID, name, ts: new Date(), room 
+    })
+  }
 
   return <main>
+    {showCamera && <Camera takePicture={takePicture} />}
     <header> 
       <div style={{display:'flex',alignItems:'center'}}>
         <img 
@@ -33,33 +53,57 @@ function Room(props) {
     </header>
 
     <div className="messages" /* we want to make a scrollable block */> 
-    {messages.map((m,i)=>{ /* map is a built in function that allows you to loop through the array*/
-      return <div key={i} className="messages-wrap" from={m.name===name?'me':'you'}>
-        <div className="msg">
-          <div className="msg-name">{m.name}</div>
-          <div className="msg-text">{m.text}</div>
-        </div>
-      </div>
-    })}
+      {messages.map((m,i)=> <Message key={i} m={m} name={name}/>)}
     </div>
 
-    <TextInput onSend={(text)=> {
-      db.send({
-        text, name, ts: new Date(), room
-      })
-    }}/>
+    <TextInput 
+      showCamera={()=>setShowCamera(true)}
+      onSend={(text)=> {
+        db.send({
+          text, name, ts: new Date(), room
+        })
+      }}
+    />
   </main>
 }
 
+const bucket = 'https://firebasestorage.googleapis.com/v0/b/chatter2020-bf8ab.appspot.com/o/'
+const suffix = '.jpg?alt=media'
+
+function Message({m, name}){
+  return <div className="messages-wrap" 
+    from={m.name===name?'me':'you'} 
+    onClick={()=>console.log(m)}>
+    <div className="msg">
+      <div className="msg-name">{m.name}</div>
+      <div className="msg-text">
+        {m.text}
+        {m.img && <img src={bucket + m.img + suffix} alt="pic" />}
+      </div>         
+    </div>
+  </div>
+}
+
 function TextInput(props) {
-  const [text, setText] = useState('') /* empty string as initial state, setText as a function to set the text*/ 
+  var [text, setText] = useState('') /* empty string as initial state, setText as a function to set the text*/ 
+  
   return <div className="text-input-wrap">
+    <button onClick={props.showCamera} className="camera">
+      <FiCamera className="camera-img" />
+    </button>
     <input className="text-input"
       value={text}
       placeholder = "Write your message"
       onChange = {e=> setText(e.target.value)}
+      onKeyPress={e=> {
+        if(e.key==='Enter') {
+          if(text) props.onSend(text)
+          setText('')
+        }
+      }}
     />
     <button className="button"
+      disabled={!text}
       onClick={()=> {
       if(text) props.onSend(text) /* 'text' is like the parameters sent to the function onSend, which shows as 'm'*/
       setText('')
